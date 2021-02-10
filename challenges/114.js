01847bc353f168f62b20e3e12a432c88bb94ff23
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-02-10 15:32:30 -0800

    flat to Infinity

--- challenges/114.js
+++ challenges/114.js
@@ -21,7 +21,7 @@ function flatten(array){
     // your code here
     let flattened = array.reduce(
         function(accumulator, currentValue) {
-          return accumulator.flat(2).concat(currentValue)
+          return accumulator.flat(Infinity).concat(currentValue)
         },
         []
       )
@@ -35,27 +35,10 @@ console.log(flatten(a));
 //
 // Bonus: make it work even with a nested array like:
 //
-let nested = [[1,2],[3, [4, 5]], [6], 7]
-
+let nested = [[1,2],[3, [4, 5]], [6], 7];
 console.log(flatten(nested));
-//
-// // [1,2,3,4,5,6,7]
-flatten([[1,9],[3,8]]);
-// should return [1,9,3,8]
-console.log(flatten([[1,9],[3,8]]).length);
-    //   Expected $.length = 2 to equal 4.
-    console.log(flatten([[1,9],[3,8]])[0]);
-    //   Expected $[0] = [ 1, 9 ] to equal 1.
-    console.log(flatten([[1,9],[3,8]])[1]);
-    //   Expected $[1] = [ 3, 8 ] to equal 9.
-    console.log(flatten([[1,9],[3,8]])[2]);
-    //   Expected $[2] = undefined to equal 3.
-    console.log(flatten([[1,9],[3,8]])[3]);
-    //   Expected $[3] = undefined to equal 8.
-    // Stack:
-    //   Error: Expected $.length = 2 to equal 4.
-    //   Expected $[0] = [ 1, 9 ] to equal 1. 
-    //   Expected $[1] = [ 3, 8 ] to equal 9. 
-    //   Expected $[2] = undefined to equal 3. 
-    //   Expected $[3] = undefined to equal 8. 
+
+let b = [[1,9],[3,8]];
+console.log(flatten(b));
+    //   expect(flatten(b)).toEqual([1,9,3,8]);
 module.exports = flatten;
